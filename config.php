9fd6308cdd6c5e138d1547a96d6e022084fa396b
Depdog <89854913+Depdog@users.noreply.github.com>
2023-04-11 17:27:10 -0400

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@
 $config = array();
 
 // a unique key that identifies this application - DO NOT LEAVE THIS EMPTY!
-$config['app_key'] = '';
+$config['app_key'] = 'dwefrt4frefdsaw';
 
 // a secret key to be used during encryption
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'hhrfreh3bbdw';
 
 // URL running the proxy app
 //$config['app_url'] = 'https://www.mysampleproxy.com/proxyfolder/';
